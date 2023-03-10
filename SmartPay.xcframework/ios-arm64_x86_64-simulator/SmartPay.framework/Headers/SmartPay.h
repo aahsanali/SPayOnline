12741548c760bdb53f4300900202be3fe2d2acba
Naveed A <naveed.ahsan@coeus-solutions.de>
2023-03-10 13:54:34 +0100

    framework v0.0.1

---
+++ SmartPay.xcframework/ios-arm64_x86_64-simulator/SmartPay.framework/Headers/SmartPay.h
@@ -0,0 +1,19 @@
+//
+//  SmartPay.h
+//  SmartPay
+//
+//  Created by Vadim Khomenok on 8/27/20.
+//  Copyright © 2020 NS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmartPay.
+FOUNDATION_EXPORT double SmartPayVersionNumber;
+
+//! Project version string for SmartPay.
+FOUNDATION_EXPORT const unsigned char SmartPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartPay/PublicHeader.h>
+
+
