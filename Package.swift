679aaa4f45ac7fb1279fb7798296038bcf8b00a9
Naveed Ahsan <naveed.ahsan@coeus-solutions.de>
2023-03-14 13:01:12 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,21 +6,19 @@ import PackageDescription
 let package = Package(
     name: "SmartPay",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SmartPay",
-            targets: ["SmartPayBinary"]),
+            targets: ["SmartPay"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "SmartPay"),
         .binaryTarget(
-            name: "SmartPayBinary",
-            url: "https://raw.githubusercontent.com/aahsanali/SPayOnline/develop/SmartPay.framework.zip",
-            checksum: "b209878522db27be8d89dcadbe085a74258ad08c57ef1b08fa47f8d2ee6ef9f7"),
+            name: "SmartPay",
+            url: "https://raw.githubusercontent.com/aahsanali/SPayOnline/develop/SmartPay.zip",
+            checksum: "b1602624a1852049433a7a9a4a666bd1b881625234da4389c0f56473f07f476c")
     ]
 )
